96b9113d7f1d21ce863b361fabaaad5c6e7f4945
IrisSaskia <irisoostra@upcmail.nl>
2019-03-01 11:57:59 +0100

    Gele invader
    Hop hop hop bunny hop

--- MonoGameInvaders/YellowInvader.cs
+++ MonoGameInvaders/YellowInvader.cs
@@ -6,8 +6,27 @@ using System.Threading.Tasks;
 
 namespace MonoGameInvaders {
 	class YellowInvader : Invader {
+		public int frameCounter = 0;
+
 		public YellowInvader() : base("sprites/enemies/spr_yellow_invader") {
 
+		}
+
+		public override void Init() {
+			base.Init();
+			velocity.X = 2.5f;
+			velocity.Y = 15;
+		}
+
+		public override void Update() {
+			base.Update();
+			frameCounter++;
+			if(frameCounter == 10) {
+				position.Y += velocity.Y;
+			} else if(frameCounter == 20) {
+				position.Y -= velocity.Y;
+				frameCounter = 0;
+			}
 		}
 
 	}
